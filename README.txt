675c536f9eb7f9799dcc254d0424741adc50283b
Tom Neyland <tcneyland@middos-kubuntu-box.(none)>
2011-09-02 13:33:22 -0400

    Added installer

---
+++ README.txt
@@ -0,0 +1,26 @@
+pyreddit
+--------
+
+PyReddit is a wrapper for Reddit.com's Json API
+
+--------
+
+A Quick todo list:
+----
+
+* Create documentation
+* Write examples
+* Write tests
+* Wrap the remaining parts of the reddit API
+
+Dependencies
+----
+
+pyreddit.core
+
+* mechanize
+
+pyreddit.crawlers
+
+* gevent
+* mechanize
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distribute_setup import use_setuptools
+use_setuptools()
+from setuptools import setup
+
+setup(
+    name='PyReddit',
+    version='0.1.1dev',
+    packages=['pyreddit',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+    install_requires=['mechanize'],
+)
